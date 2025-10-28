8dc1b8044a0db8cd90a6a02bc74003f6ec78c6bb
Chandan Pandey <chandanpandey1928@gmail.com>
2025-10-28 19:51:27 +0530

    Validating guru99 banking domain website with cucumber

--- src/test/java/StepDefinition/StepDefinition.java
+++ src/test/java/StepDefinition/StepDefinition.java
@@ -2,6 +2,7 @@ package StepDefinition;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -23,26 +24,26 @@ public class StepDefinition {
 
 	@When("user enters username")
 	public void user_enters_username() {
-	   
+	   driver.findElement(By.xpath("//input[@name='uid']")).sendKeys("mngr643521");
 	}
 
 	@When("user enters password")
 	public void user_enters_password() {
-	    
+	   driver.findElement(By.xpath("//input[@name='password']")).sendKeys("esypapE");
 	}
 
 	@When("user clicks on submit button")
 	public void user_clicks_on_submit_button() {
-	    
+	    driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
 	}
 
 	@When("user credentials are wrong")
 	public void user_credentials_are_wrong() {
-	    
+	    System.out.println("For negative testing");
 	}
 
 	@Then("an error message should be displayed on webpage")
 	public void an_error_message_should_be_displayed_on_webpage() {
-
+	//	driver.switchTo().alert().accept();
 	}	
 }
