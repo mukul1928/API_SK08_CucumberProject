f18fea7685e0e638c2a4c407a8d78c3d32e52c4a
Chandan Pandey <chandanpandey1928@gmail.com>
2025-10-29 13:07:56 +0530

    Cucumber Background

--- src/test/java/FeatureFiles/TestScenario.feature
+++ src/test/java/FeatureFiles/TestScenario.feature
@@ -1,14 +1,16 @@
 Feature: Validate the items added to the cart
 
-  Scenario: Validate login
+  Background: 
     Given user logins into the web page
     And user enters credentials
     And user clicks on submit button
 
+  Scenario: Validate login
+    And user navigates to homepage of application
+    Then user verifies the page title
+
+  @sanity
   Scenario: Validate addition to cart
-    Given user logins into the web page
-    And user enters credentials
-    And user clicks on submit button
     And user searches as item
     And user adds the item at the cart
     Then user verifies item added to cart
--- src/test/java/StepDefinition/StepDefinition.java
+++ src/test/java/StepDefinition/StepDefinition.java
@@ -11,41 +11,50 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class StepDefinition {
-	
+
 	@Before
 	public void beforeTest() {
 		System.out.println("This is before hooks");
 	}
-	
+
 	@After
 	public void afterTest() {
 		System.out.println("This is after hooks");
 	}
-	
-	
+
+	@Before("@sanity")
+	public void beforeTest1() {
+		System.out.println("I will only run before tag sanity");
+	}
+
+	@After("@sanity")
+	public void afterTest1() {
+		System.out.println("I will only run after tag sanity");
+	}
+
 	WebDriver driver;
-	
+
 	@Given("user is on the login page")
 	public void user_is_on_the_login_page() {
 		driver = new ChromeDriver();
 		driver.get("https://demo.guru99.com/V4/index.php");
 		driver.manage().window().maximize();
-	//	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		// driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
 
 	@When("user enters username")
 	public void user_enters_username() {
-	   driver.findElement(By.xpath("//input[@name='uid']")).sendKeys("mngr643521");
+		driver.findElement(By.xpath("//input[@name='uid']")).sendKeys("mngr643521");
 	}
 
 	@When("user enters password")
 	public void user_enters_password() {
-	   driver.findElement(By.xpath("//input[@name='password']")).sendKeys("esypapE");
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("esypapE");
 	}
 
 	@When("user clicks on submit button")
 	public void user_clicks_on_submit_button() {
-	   // driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
+		// driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
 		System.out.println("submit button clicked");
 	}
 
@@ -57,41 +66,51 @@ public class StepDefinition {
 	 * an_error_message_should_be_displayed_on_webpage() {
 	 * driver.switchTo().alert().accept(); }
 	 */
-	
+
 	@When("user enters username as {string}")
 	public void user_enters_username_as(String username) {
-		 driver.findElement(By.xpath("//input[@name='uid']")).sendKeys(username);
+		driver.findElement(By.xpath("//input[@name='uid']")).sendKeys(username);
 	}
 
 	@When("user enters password as {string}")
 	public void user_enters_password_as(String password) {
-		 driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
-		
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
+
 	}
-	
+
 	@Given("user logins into the web page")
 	public void user_logins_into_the_web_page() {
-	    System.out.println("Login succesfull");
+		System.out.println("Login succesfull");
 	}
 
 	@Given("user enters credentials")
 	public void user_enters_credentials() {
-	    System.out.println("credentials enters");
+		System.out.println("credentials enters");
 	}
 
 	@Given("user searches as item")
 	public void user_searches_as_item() {
-	    System.out.println("item searched");
+		System.out.println("item searched");
 	}
 
 	@Given("user adds the item at the cart")
 	public void user_adds_the_item_at_the_cart() {
-	    System.out.println("item added");
+		System.out.println("item added");
 	}
 
 	@Then("user verifies item added to cart")
 	public void user_verifies_item_added_to_cart() {
-	    System.out.println("validation successfull");
+		System.out.println("validation successfull");
+	}
+	
+	@Given("user navigates to homepage of application")
+	public void user_navigates_to_homepage_of_application() {
+			System.out.println("Navigation to home page successfull");
+	  	}
+
+	@Then("user verifies the page title")
+	public void user_verifies_the_page_title() {
+	   System.out.println("page title verified");
 	}
 
 }
